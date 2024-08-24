0d87baf84e32aba30ff0d8cf1cc3f3d8e2399451
MahmudulRafi <rafi.mahmudul.h@gmail.com>
2024-08-25 03:56:48 +0600

    fix : table structure modify

--- script.js
+++ script.js
@@ -10,29 +10,26 @@ $(document).ready(function () {
     url: sheetURL,
     dataType: "text",
     success: function (response) {
-      // var data = $.csv.toArrays(response);
+
       var sheetData = $.csv.toObjects(response);
-      console.log(sheetData);
 
-      var html = "<table id='list-table'>";
-      html += "<tr><th>সংগঠনের/প্রদানকারীর নাম</th><th>পণ্যের বিবরণ</th><th>পরিমান (জন)</th><th>বিতরণের প্রস্তাবিত স্থান</th><th>প্রতিনিধিগনের নাম ও মোবাইল নং</th></tr>";
+      var tableHeader = "<tr><th>সংগঠনের/প্রদানকারীর নাম</th><th>পণ্যের বিবরণ</th><th>পরিমান (জন)</th><th>বিতরণের প্রস্তাবিত স্থান</th><th>প্রতিনিধিগনের নাম ও মোবাইল নং</th></tr>";
+ 
+      $("#list-table thead").append(tableHeader);
 
-      // Iterate over each row of data and add it to the table
       sheetData.forEach(function (row) {
-        html += "<tr>";
-        html += `<td>${row['সংগঠনের / প্রদানকারীর নাম']}</td>`;
-        html += `<td>${row['পণ্যের বিবরণ']}</td>`;
-        html += `<td>${row['পরিমাণ (জন)']}</td>`;
-        html += `<td>${row['বিতরণের জন্য প্রস্তাবিত স্থান']}</td>`;
-        html += `<td>${row['প্রতিনিধির নাম ও মোবাইল নং']}</td>`;
-        html += "</tr>";
+        var tableRow = "";
+        tableRow += "<tr>";
+        tableRow += `<td>${row['সংগঠনের / প্রদানকারীর নাম']}</td>`;
+        tableRow += `<td>${row['পণ্যের বিবরণ']}</td>`;
+        tableRow += `<td>${row['পরিমাণ (জন)']}</td>`;
+        tableRow += `<td>${row['বিতরণের জন্য প্রস্তাবিত স্থান']}</td>`;
+        tableRow += `<td>${row['প্রতিনিধির নাম ও মোবাইল নং']}</td>`;
+        tableRow += "</tr>";
+        
+        $("#list-table tbody").append(tableRow);
+
       });
-
-      html += "</table>";
-
-      // Append the generated table to the #list div
-      $('#list').html(html);
-
     },
   });
 });
