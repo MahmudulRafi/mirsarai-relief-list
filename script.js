a0d0cb7685091c0b8db3f46930f9b1192970ef4f
MahmudulRafi <rafi.mahmudul.h@gmail.com>
2024-08-25 10:44:30 +0600

    fix : product list style change

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
   const sheetId = "1k4t4Ylm4UduGMjV2OY4Sn7bSsUTBYxysECc-y-JcLw8";
   // sheetName is the name of the TAB in your spreadsheet (default is "Sheet1")
   const sheetName = encodeURIComponent("25.8");
@@ -10,25 +9,29 @@ $(document).ready(function () {
     url: sheetURL,
     dataType: "text",
     success: function (response) {
-
       var sheetData = $.csv.toObjects(response);
 
-      var tableHeader = "<tr><th>সংগঠনের/প্রদানকারীর নাম</th><th>পণ্যের বিবরণ</th><th>পরিমান (জন)</th><th>বিতরণের প্রস্তাবিত স্থান</th><th>প্রতিনিধিগনের নাম ও মোবাইল নং</th></tr>";
- 
+      var tableHeader =
+        "<tr><th>সংগঠনের/প্রদানকারীর নাম</th><th>পণ্যের বিবরণ</th><th>পরিমান (জন)</th><th>বিতরণের প্রস্তাবিত স্থান</th><th>প্রতিনিধিগনের নাম ও মোবাইল নং</th></tr>";
+
       $("#list-table thead").append(tableHeader);
 
       sheetData.forEach(function (row) {
         var tableRow = "";
         tableRow += "<tr>";
-        tableRow += `<td>${row['সংগঠনের / প্রদানকারীর নাম']}</td>`;
-        tableRow += `<td>${row['পণ্যের বিবরণ']}</td>`;
-        tableRow += `<td>${row['পরিমাণ (জন)']}</td>`;
-        tableRow += `<td>${row['বিতরণের জন্য প্রস্তাবিত স্থান']}</td>`;
-        tableRow += `<td>${row['প্রতিনিধির নাম ও মোবাইল নং']}</td>`;
-        tableRow += "</tr>";
-        
-        $("#list-table tbody").append(tableRow);
+        tableRow += `<td>${row["সংগঠনের / প্রদানকারীর নাম"]}</td>`;
 
+        var items = row['পণ্যের বিবরণ'].split(","); // Adjust the delimiter if necessary
+        var squareIcon = "•"; // Square-shaped icon
+        var rowList = items.map(item => `${squareIcon} ${item.trim()}`).join(" "); // Add square before each item and join them
+      
+        tableRow += `<td>${rowList}</td>`;
+        tableRow += `<td>${row["পরিমাণ (জন)"] || "N/A"}</td>`;
+        tableRow += `<td>${row["বিতরণের জন্য প্রস্তাবিত স্থান"]}</td>`;
+        tableRow += `<td>${row["প্রতিনিধির নাম ও মোবাইল নং"]}</td>`;
+        tableRow += "</tr>";
+
+        $("#list-table tbody").append(tableRow);
       });
     },
   });
